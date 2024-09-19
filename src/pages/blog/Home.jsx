d35e888eade7b5db3d53166e5353cd28e1271d97
singhaarati <aarati5737@gmail.com>
2024-09-19 16:50:03 +0545

    learning tailwind and react

---
+++ src/pages/blog/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Layout from '../../components/layout/Layout'
+import Card from './components/card/Card'
+
+const Home = () => {
+    return (
+        <Layout>
+            <div className='flex flex-wrap justify-center space-x-5 mt-6
+             m'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            </div>
+        </Layout>
+    )
+}
+
+export default Home
