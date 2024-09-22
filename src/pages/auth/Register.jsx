e2a9b9d514c7ffd18fbc028d8887c3cd9170ae83
singhaarati <aarati5737@gmail.com>
2024-09-22 16:45:40 +0545

    setState used in login and register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Form from './components/form/Form'
 
 const Register = () => {
+    const handleRegister = (data) => {
+        console.log(data,"Inside Handle")
+    }
     return (
-        <Form type='Register' />
+        <Form type='Register' onSubmit={handleRegister} />
     )
 }
 
--- src/pages/auth/components/form/Form.jsx
+++ src/pages/auth/components/form/Form.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = ({ type }) => {
+const htmlForm = ({ type, onSubmit }) => {
+    const [data, setData] = useState({
+        email: '',
+        username: '',
+        password: '',
+    })
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        console.log(name, value)
+        setData({
+            ...data,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        onSubmit(data)
+    }
+
+
+
     return (
         <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div
-                    className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                    className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transhtmlForm -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
                 </div>
+
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                     <div className="max-w-md mx-auto">
                         <div>
@@ -14,30 +36,37 @@ const Form = ({ type }) => {
                                 {type === 'Login' ? 'Login here to continue...' : 'Register here to continue...'}
                             </h1>
                         </div>
-                        <div className="divide-y divide-gray-200">
-                            <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                <div className="relative">
-                                    <input autocomplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-                                    <label for="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
-                                </div>
-                                {
-                                    type === 'Register' && (
-                                        <div className="relative">
-                                            <input autocomplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-                                            <label for="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-                                        </div>
-                                    )}
+                        <form onSubmit={handleSubmit}>
+                            <div className="divide-y divide-gray-200">
+                                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                    <div className="relative">
+                                        <input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" onChange={handleChange} required />
+                                        <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+                                    </div>
+                                    {
+                                        type === 'Register' && (
+                                            <div className="relative">
+                                                <input autoComplete="off" id="username" name="username" type="username" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username address" onChange={handleChange} required />
+                                                <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username</label>
+                                            </div>
+                                        )
+                                    }
+                                    <div className="relative">
+                                        <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange={handleChange} required />
+                                        <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+                                    </div>
 
-                                <div className="relative">
-                                    <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                                    <div className="relative">
+                                        <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
-export default Form
+export default htmlForm
